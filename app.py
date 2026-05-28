24be44c0778358268cdedf2d60fbe4eed6a833e6
Naman Kumar <namankumar1170@gmail.com>
2026-05-28 09:05:46 +0530

    commit

--- app.py
+++ app.py
@@ -19,7 +19,18 @@ with st.sidebar.form("predict_form"):
     volume_mcap_ratio = st.number_input("volume_mcap_ratio", value=0.0, format="%.10f")
 
     submit = st.form_submit_button(" Click To Predict")
+with st.expander("about us"):
+    st.write("""
+    **Crypto Volatility Predictor** is a project developed by a team of data scientists and cryptocurrency enthusiasts. Our mission is to provide accurate and actionable insights into the volatility of cryptocurrencies, helping traders and investors make informed decisions. We leverage advanced machine learning techniques and a comprehensive dataset to predict future volatility based on historical trends and engineered features. Our goal is to empower users with the tools they need to navigate the dynamic world of cryptocurrency trading with confidence.
+    """)    
 
+with st.expander("Developer"):
+    st.write("""
+    **Developer**: [Naman Kumar]
+    
+    **GitHub**: [https://github.com/Naman27-bit]
+    **Email**: [namankumar1170@gmail.com]
+    """)
 if submit:
     payload = {
         "volatility_7": float(volatility_7),
